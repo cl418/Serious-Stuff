db5aa10bdbd22c222d38c738210c48c01600bb00
Karen Chen <karencyuan@gmail.com>
2020-05-26 23:44:13 -0700

    Fix nit.

--- serious_stuff/lib/constants.dart
+++ serious_stuff/lib/constants.dart
@@ -5,4 +5,3 @@ import 'package:flutter/material.dart';
 final Color primaryColor = new Color(0xffc62828);
 final Color primaryColorDark = new Color(0xff8e0000);
 final Color primaryColorLight = new Color(0xffff5f52);
-
