d8a32fd7ee5810034084e9794fd86c404b79ba8e
Karen Chen <karencyuan@gmail.com>
2020-05-26 23:37:40 -0700

    Add FAB and constants.

---
+++ serious_stuff/lib/components/Fab.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:serious_stuff/constants.dart' as Constants;
+
+SpeedDial buildOptionsFAB() {
+  // The options floating action button.
+  return SpeedDial(
+    marginRight: 18,
+    marginBottom: 20,
+    animatedIcon: AnimatedIcons.menu_close,
+    animatedIconTheme: IconThemeData(size: 22.0),
+    visible: true,
+    closeManually: false,
+    curve: Curves.bounceIn,
+    overlayColor: Colors.black,
+    overlayOpacity: 0.5,
+    onOpen: () => print('OPENING DIAL'),
+    onClose: () => print('DIAL CLOSED'),
+    tooltip: 'Options',
+    heroTag: 'options-fab',
+    backgroundColor: Colors.white,
+    foregroundColor: Colors.black,
+    elevation: 8.0,
+    shape: CircleBorder(),
+    children: [
+      SpeedDialChild(
+          child: Icon(Icons.search),
+          backgroundColor: Constants.primaryColorLight,
+          label: 'Search a room',
+          labelStyle: TextStyle(fontSize: 16.0),
+          onTap: () => print('SEARCH')),
+      SpeedDialChild(
+        child: Icon(Icons.settings),
+        backgroundColor: Constants.primaryColorLight,
+        label: 'Settings',
+        labelStyle: TextStyle(fontSize: 16.0),
+        onTap: () => print('SETTINGS'),
+      ),
+    ],
+  );
+}
---
+++ serious_stuff/lib/constants.dart
@@ -0,0 +1,8 @@
+library constants;
+
+import 'package:flutter/material.dart';
+
+final Color primaryColor = new Color(0xffc62828);
+final Color primaryColorDark = new Color(0xff8e0000);
+final Color primaryColorLight = new Color(0xffff5f52);
+
