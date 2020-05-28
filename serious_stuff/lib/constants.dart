9bbdc6bdcc99e81a09052f5ee6e716fcfc691282
Karen Chen <karencyuan@gmail.com>
2020-05-27 21:31:52 -0700

    Refactor code.

--- serious_stuff/lib/constants.dart
+++ serious_stuff/lib/constants.dart
@@ -5,3 +5,5 @@ import 'package:flutter/material.dart';
 final Color primaryColor = new Color(0xffc62828);
 final Color primaryColorDark = new Color(0xff8e0000);
 final Color primaryColorLight = new Color(0xffff5f52);
+final Color primaryBackgroundColor = Colors.white;
+final Color primaryForegroundColor = Colors.black;
